a82c609675479860ee02ad9aeade1c4cc62a68ab
jjjohny228 <gggleb13@gmail.com>
2024-06-22 08:57:22 +0300

    added user authentication

--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(
-        "api/restaurant/",
-        include("restaurant.urls",
-                namespace="restaurant")
-    ),
+    path('api/restaurant/', include("restaurant.urls", namespace="restaurant")),
+    path('api/user/', include("user.urls", namespace="user")),
 ]
